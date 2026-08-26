52e188ca8de02e8c01eb206be13d0730562e65a7
Saturn Labs <anunes.line@gmail.com>
2026-08-26 15:25:46 -0300

    Map server cooldown to household domain

--- app/lib/features/household_routines/data/repositories/firestore_household_task_reminder_repository.dart
+++ app/lib/features/household_routines/data/repositories/firestore_household_task_reminder_repository.dart
@@ -12,11 +12,25 @@ class FirestoreHouseholdTaskReminderRepository
 
   @override
   Future<void> saveReminder(HouseholdTaskReminder reminder) async {
-    await functions.httpsCallable('createHouseholdReminder').call({
-      'reminderId': reminder.id,
-      'taskId': reminder.taskId,
-      'remindAt': reminder.remindAt.toUtc().toIso8601String(),
-    });
+    try {
+      await functions.httpsCallable('createHouseholdReminder').call({
+        'reminderId': reminder.id,
+        'taskId': reminder.taskId,
+        'remindAt': reminder.remindAt.toUtc().toIso8601String(),
+      });
+    } on FirebaseFunctionsException catch (error) {
+      if (error.code == 'resource-exhausted') {
+        final details = error.details;
+        final rawSeconds = details is Map ? details['retryAfterSeconds'] : null;
+        final seconds = rawSeconds is num
+            ? rawSeconds.ceil()
+            : int.tryParse(rawSeconds?.toString() ?? '');
+        throw HouseholdReminderCooldownException(
+          Duration(seconds: seconds != null && seconds > 0 ? seconds : 1),
+        );
+      }
+      rethrow;
+    }
   }
 
   @override
